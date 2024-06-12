dae6df6e9cd8384e9801e3f32fc61009586ff2fa
Dexter <dex-hon-sci@outlook.com>
2024-06-13 02:39:00 +0800

    File reorganisation and clean up

--- website/auth.py
+++ website/auth.py
@@ -6,18 +6,18 @@ Created on Thu Sep 21 12:56:46 2023
 @author: dexter
 """
 
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for
 
 auth = Blueprint("auth", __name__)
 
 @auth.route('/login')
 def login():
-    return "Login"
+    return render_template("login.html")
 
 @auth.route('/sign-up')
 def sigh_up():
-    return "Sigh up"
+    return render_template("signup.html")
 
 @auth.route('/logout')
 def logout():
-    return "Logout"
+    return redirect(url_for("views.home"))
